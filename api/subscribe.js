8cbfaab9d5fe20511404db8ae0a11fbaceaf0744
midooo-tech <mamdou7.khalifa@gmail.com>
2024-10-26 04:11:03 +0300

    d

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,4 +1,6 @@
-import { edgeConfig } from '@vercel/edge-config';
+// src/pages/api/subscribe.js
+
+import { set } from '@vercel/edge-config';
 
 export const config = {
   runtime: 'edge',
@@ -12,13 +14,13 @@ export default async function handler(req) {
 
     const subscription = await req.json();
 
-    // Check if subscription object has expected structure
+    // Check if subscription object has the expected structure
     if (!subscription.endpoint) {
       return new Response('Invalid subscription data', { status: 400 });
     }
 
-    // Store the subscription in Edge Config using the endpoint as the key
-    await edgeConfig.set(subscription.endpoint, JSON.stringify(subscription));
+    // Store the subscription using the endpoint as the key
+    await set(subscription.endpoint, JSON.stringify(subscription));
 
     return new Response('Subscription stored', { status: 201 });
   } catch (error) {
