d9100d4a19698aa90c3693db71083ddbc5d1dcf6
midooo-tech <mamdou7.khalifa@gmail.com>
2024-10-26 04:08:41 +0300

    s

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,21 +1,28 @@
-// src/pages/api/subscribe.js
-
 import { edgeConfig } from '@vercel/edge-config';
 
-// Configure the API route to run on the Edge Runtime
 export const config = {
   runtime: 'edge',
 };
 
 export default async function handler(req) {
-  if (req.method !== 'POST') {
-    return new Response('Method Not Allowed', { status: 405 });
+  try {
+    if (req.method !== 'POST') {
+      return new Response('Method Not Allowed', { status: 405 });
+    }
+
+    const subscription = await req.json();
+
+    // Check if subscription object has expected structure
+    if (!subscription.endpoint) {
+      return new Response('Invalid subscription data', { status: 400 });
+    }
+
+    // Store the subscription in Edge Config using the endpoint as the key
+    await edgeConfig.set(subscription.endpoint, JSON.stringify(subscription));
+
+    return new Response('Subscription stored', { status: 201 });
+  } catch (error) {
+    console.error('Error handling subscribe request:', error);
+    return new Response('Internal Server Error', { status: 500 });
   }
-
-  const subscription = await req.json();
-  
-  // Store the subscription in Edge Config using the endpoint as the key
-  await edgeConfig.set(subscription.endpoint, JSON.stringify(subscription));
-
-  return new Response('Subscription stored', { status: 201 });
 }
